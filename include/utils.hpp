2230bc46177d9c8db70256cc0ca0f667ef442792
Gautier <Hybanos@orange.fr>
2025-11-25 23:35:15 +0100

    data read

---
+++ include/utils.hpp
@@ -0,0 +1,19 @@
+#include <array>
+#include <fstream>
+
+static std::array<double, N_TOTAL * 3> load_data() {
+    std::array<double, N_TOTAL * 3> data;
+
+    char *tmp = new char[N_TOTAL*3*sizeof(double)];
+
+    std::fstream f;
+    f.open("particule.data", std::ios::in | std::ios::binary);
+    f.read(tmp, N_TOTAL * 3 * sizeof(double));
+    f.close();
+
+    for (int i = 0; i < N_TOTAL * 3; i++) {
+        data[i] = ((double *)tmp)[i];
+    }
+
+    return data;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <array>
+
+#include "utils.hpp"
+
+// #define N_total 1000
+#define N_local 100
 
 int main() {
 
-    std::cout << "haha" << std::endl;
+    std::array<double, N_TOTAL * 3> data = load_data();
+
+    for (int i = 0; i < N_TOTAL; i++) {
+        std::cout << data[i * 3] << "  " << data[i * 3 + 1] << "  " << data[i * 3 + 2] << std::endl;
+    }
 
     return 0;
 }
